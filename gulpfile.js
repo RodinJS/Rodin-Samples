6960df58cc09070ed8bc971679838e002a93fd69
Grigør <g@yvn.io>
2017-03-24 18:39:44 +0400

    RO-592 #implemented samples list genration

--- gulpfile.js
+++ gulpfile.js
@@ -38,27 +38,34 @@ gulp.task('js', () => {
    return merge(tasks);
 });
 
+gulp.task('list', () => {
+	const folders_list = getFolders(scriptsPath);
+	let uri_list = {};
+	for (var i = 0; i < folders_list.length; i++) {
+		uri_list[folders_list[i]] = { 
+			url: `http://examples.rodin.io/samples/${folders_list[i]}/`,
+			git: `https://github.com/RodinJS/Rodin-Samples/tree/master/samples/${folders_list[i]}/`
+		};
+	}
+	fs.writeFile('./list.json', JSON.stringify(uri_list, null, 2) , 'utf-8');
+});
 
 gulp.task('watch', () => {
 	gulp.watch(JS, ['js']);
 });
 
-gulp.task('clean', () => {
-	return del(['./build']);
-});
-
 gulp.task('connect', () => {
 	connect.server({
 		root: './',
-		port: 9000,
+		port: 9090,
 		livereload: true
 	});
 });
 
 gulp.task('default', (done) => {
-	sequence('clean', ['js'], done);
+	sequence(['js', 'list'], done);
 });
 
 gulp.task('dev', (done) => {
-	sequence('clean', ['js', 'connect', 'watch'], done);
+	sequence(['js', 'connect', 'watch'], done);
 });
